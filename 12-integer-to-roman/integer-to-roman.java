cea5005455fc4b5ce9c2a00e0881f4cecb5f8a43
SnekaRamya <snekaramya661@gmail.com>
2026-05-13 22:19:09 +0530

    Time: 5 ms (22.68%) | Memory: 46.3 MB (60.34%) - LeetSync

---
+++ 12-integer-to-roman/integer-to-roman.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String intToRoman(int num) {
+        int[] no={1000,900,500,400,100,90,50,40,10,9,5,4,1};
+        String[] sym={"M","CM","D","CD","C","XC","L","XL","X","IX","V","IV","I"};
+        String result="";
+         for (int i = 0; i < no.length; i++) {
+            while (num >= no[i]) {
+                result += sym[i];
+                num -= no[i];
+            }
+        }
+
+        return result;
+    }
+}
